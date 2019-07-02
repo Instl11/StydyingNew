cdf0ac893e0baf7db35f4a68d255dfb026b60bcc
Yaroslav Lynev <xxx@mail.ru>
2019-07-02 18:47:20 +0300

    Add JUnit 5

--- src/main/java/sorting/SortRunner.java
+++ src/main/java/sorting/SortRunner.java
@@ -8,7 +8,7 @@ import static sorting.Sorts.sortWithSelection;
 // Это не тест =) Читай про ЮнитТесты
 public class SortRunner {
     public static void main(String[] args) {
-        int[] arr1 = new int[500];
+        int[] arr1 = new int[50000];
         Random random = new Random();
         for (int i = 0; i < arr1.length; i++)
             arr1[i] = random.nextInt(1000);
@@ -16,8 +16,8 @@ public class SortRunner {
         int[] arr3 = Arrays.copyOf(arr1, arr1.length);
         int[] arr4 = Arrays.copyOf(arr1, arr1.length);
 
-       /* sortWithSelection(arr1, SortType.MY, true);
-        sortWithSelection(arr2, SortType.BUBBLE, true);*/
+        sortWithSelection(arr1, SortType.MY, true);
+        sortWithSelection(arr2, SortType.BUBBLE, true);
         sortWithSelection(arr3, SortType.QUICK, true);
         sortWithSelection(arr4, SortType.JAVABOX, true);
 
