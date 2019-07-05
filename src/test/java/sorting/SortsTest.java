5a2732457118affc1a02503db297a392cff2a9bb
Yaroslav Lynev <xxx@mail.ru>
2019-07-05 18:09:16 +0300

    Added time counting in testing

--- src/test/java/sorting/SortsTest.java
+++ src/test/java/sorting/SortsTest.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -14,13 +16,19 @@ class SortsTest {
 
     private int[] arr;
     private int[] copyArr;
+    private long start;
+
+    @BeforeEach
+    void start(){
+        start = System.currentTimeMillis();
+    }
 
     @ParameterizedTest
     @DisplayName("Method with sort selection")
     @EnumSource(SortType.class)
     void testSortWithSelection(SortType sortType) {
         arraysInit(10000);
-        sortWithSelection(arr, sortType, true);
+        sortWithSelection(arr, sortType, false);
         Arrays.sort(copyArr);
         assertArrayEquals(this.copyArr, arr, "The array is not sorted");
     }
@@ -32,5 +40,10 @@ class SortsTest {
             arr[i] = random.nextInt(10000) - 5000;
         }
         copyArr = Arrays.copyOf(arr, arr.length);
+    }
+
+    @AfterEach
+    void end(){
+        System.out.println("Execution time: " + (System.currentTimeMillis() - start));
     }
 }
